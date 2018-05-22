8e4127e5a1f27730c7513ad021b10d581212aed7
Senthil360 <senthilmanikandan360@gmail.com>
2018-05-22 16:59:00 +0530

    Add min and max frequencies for big and LITTLE Cpus

--- gt.py
+++ gt.py
@@ -33,11 +33,18 @@ def file_contents(x, y):
    x.close()
    del a, b, y
 
+
 little_freq = (file_contents("/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies", "z"))
+
+little_min_freq = (little_freq[0])
+little_max_freq = (little_freq[-1])
 
 little_gov = (file_contents("/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor", "z"))
 
 big_freq = (file_contents("/sys/devices/system/cpu/cpu2/cpufreq/scaling_available_frequencies", "z"))
+
+big_min_freq = (big_freq[0])
+big_max_freq = (big_freq[-1])
 
 big_gov = (file_contents("/sys/devices/system/cpu/cpu2/cpufreq/scaling_governor", "z"))
 
@@ -46,18 +53,11 @@ big_gov = (file_contents("/sys/devices/system/cpu/cpu2/cpufreq/scaling_governor"
 import os
 little_gov_dir = "/sys/devices/system/cpu/cpu0/cpufreq/%s" % little_gov
 little_tunables = (os.listdir(little_gov_dir))
-print(little_tunables)
 #little cpu governor tunables are stored in 'little_tunables'
 
-import os
 big_gov_dir = "/sys/devices/system/cpu/cpu2/cpufreq/%s" % big_gov
 big_tunables = (os.listdir(big_gov_dir))
-print(big_tunables)
 #big cpu governor tunables are stored in 'big_tunables'
-
-
-#TO-DO Write the above stuff in functions
-
 
 
 
